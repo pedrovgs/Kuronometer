169587961b2245d357545c5bc5a1d33180b0091f
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-05-17 16:56:13 +0200

    Update mother implementation to avoid problems with the values initialization

--- kuronometer-core/src/test/scala/com/github/pedrovgs/kuronometer/mothers/SummaryBuildStagesExecutionMother.scala
+++ kuronometer-core/src/test/scala/com/github/pedrovgs/kuronometer/mothers/SummaryBuildStagesExecutionMother.scala
@@ -6,14 +6,16 @@ object SummaryBuildStagesExecutionMother {
 
   val anEmptySummary: SummaryBuildStagesExecution = SummaryBuildStagesExecution()
 
+  val anyExecutionTime: Long = 2000000
+
+  val anyExecutionTimestamp: Long = 123456789L
+
   val anyNonEmptySummary: SummaryBuildStagesExecution = {
     val stage1 = SummaryBuildStageExecution("compile", anyExecutionTime, anyExecutionTimestamp)
     val stage2 = SummaryBuildStageExecution("test", anyExecutionTime, anyExecutionTimestamp)
     SummaryBuildStagesExecution(Seq(stage1, stage2))
   }
 
-  val anyExecutionTimestamp: Long = 123456789L
 
-  val anyExecutionTime: Long = 2000000
 
 }
