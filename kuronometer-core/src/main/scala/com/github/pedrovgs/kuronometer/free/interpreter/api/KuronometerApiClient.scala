16d69b388c34273002d75dcbd6cb49c826a78d93
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-05-21 21:17:39 +0200

    Change API client implementation to use Try apply method

--- kuronometer-core/src/main/scala/com/github/pedrovgs/kuronometer/free/interpreter/api/KuronometerApiClient.scala
+++ kuronometer-core/src/main/scala/com/github/pedrovgs/kuronometer/free/interpreter/api/KuronometerApiClient.scala
@@ -1,11 +1,12 @@
 package com.github.pedrovgs.kuronometer.free.interpreter.api
 
-import com.github.pedrovgs.kuronometer.KuronometerResults.{UnknownError, ConnectionError, KuronometerResult}
+import com.github.pedrovgs.kuronometer.KuronometerResults.{ConnectionError, KuronometerResult, UnknownError}
 import com.github.pedrovgs.kuronometer.free.domain.{BuildExecution, Platform}
 import net.liftweb.json.DefaultFormats
 import net.liftweb.json.Serialization.write
 import net.liftweb.json.ext.EnumNameSerializer
 
+import scala.util.Try
 import scalaj.http.Http
 
 class KuronometerApiClient {
@@ -20,17 +21,12 @@ class KuronometerApiClient {
 
   private def sendPostRequest(buildExecution: BuildExecution, body: String, path: String)
                              (implicit apiClientConfig: KuronometerApiClientConfig): KuronometerResult[BuildExecution] = {
-    val response = Http(composeUrl(path))
+    Try(Http(composeUrl(path))
       .headers(KuronometerApiClientConfig.headers)
       .postData(body)
-      .asString
-    if (response.isSuccess) {
-      Right(buildExecution)
-    } else if (response.is5xx) {
-      Left(UnknownError)
-    } else {
-      Left(ConnectionError)
-    }
+      .asString)
+      .map(response => if (response.isSuccess) Right(buildExecution) else Left(UnknownError))
+      .toOption.getOrElse(Left(ConnectionError))
   }
 
   private def composeUrl(path: String)(implicit apiClientConfig: KuronometerApiClientConfig): String = apiClientConfig.scheme + "://" + apiClientConfig.host + ":" + apiClientConfig.port + path
