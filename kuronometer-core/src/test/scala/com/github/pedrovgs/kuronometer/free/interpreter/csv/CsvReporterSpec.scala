4bae8a61358aaee6053d381d168c0937f6e49329
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-05-22 00:21:50 +0200

    Add the last test before to merge this branch

--- kuronometer-core/src/test/scala/com/github/pedrovgs/kuronometer/free/interpreter/csv/CsvReporterSpec.scala
+++ kuronometer-core/src/test/scala/com/github/pedrovgs/kuronometer/free/interpreter/csv/CsvReporterSpec.scala
@@ -3,7 +3,7 @@ package com.github.pedrovgs.kuronometer.free.interpreter.csv
 import java.io.File
 
 import com.github.pedrovgs.kuronometer.free.domain.{BuildExecution, SummaryBuildStageExecution, SummaryBuildStagesExecution}
-import com.github.pedrovgs.kuronometer.generators.BuildExecutionGenerators._
+import com.github.pedrovgs.kuronometer.generators.BuildExecutionGenerators.{buildExecution, _}
 import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
@@ -39,6 +39,18 @@ class CsvReporterSpec extends FlatSpec with Matchers with PropertyChecks with Be
       val timestamp = 500
       val filteredBuildExecutions = reporter.getBuildExecutionStagesSinceTimestamp(timestamp)
       val expectedBuildExecutions = buildExecution.buildStagesExecution.stages.filter(timestamp <= _.execution.timestamp).map(reportedStage => SummaryBuildStageExecution(reportedStage.info.name, reportedStage.execution.executionTimeInNanoseconds, reportedStage.execution.timestamp))
+      filteredBuildExecutions shouldBe Right(SummaryBuildStagesExecution(expectedBuildExecutions))
+    }
+  }
+
+  it should "count build stages with the exact timestamp as part of the filtered build stages" in {
+    val filterTimestamp = 1000
+    forAll(buildExecution(Gen.const(filterTimestamp))) { (buildExecution: BuildExecution) =>
+      clearReporterFile
+      reporter.report(buildExecution)
+      val timestamp = filterTimestamp
+      val filteredBuildExecutions = reporter.getBuildExecutionStagesSinceTimestamp(timestamp)
+      val expectedBuildExecutions = buildExecution.buildStagesExecution.stages.map(reportedStage => SummaryBuildStageExecution(reportedStage.info.name, reportedStage.execution.executionTimeInNanoseconds, reportedStage.execution.timestamp))
       filteredBuildExecutions shouldBe Right(SummaryBuildStagesExecution(expectedBuildExecutions))
     }
   }
